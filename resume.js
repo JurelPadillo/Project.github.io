3708e4e8f8b5b38dfeed8c58547a1ba8fe956979
JurelPadillo <157613405+JurelPadillo@users.noreply.github.com>
2024-02-10 17:37:23 +0800

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,24 @@
+function resume() {
+  var x = document.getElementById("Resume-Div");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function report() {
+  var y = document.getElementById("Report-Div");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
+function attendance() {
+  var z = document.getElementById("Attendace-Div");
+  if (z.style.display === "none") {
+    z.style.display = "block";
+  } else {
+    z.style.display = "none";
+  }
+}
